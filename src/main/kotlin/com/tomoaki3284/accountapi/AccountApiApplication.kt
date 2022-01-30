2b76c8e0cc6092d478987258286343743901011c
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2022-01-30 02:24:20 -0500

    first commit

---
+++ src/main/kotlin/com/tomoaki3284/accountapi/AccountApiApplication.kt
@@ -0,0 +1,11 @@
+package com.tomoaki3284.accountapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AccountApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/tomoaki3284/accountapi/AccountApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tomoaki3284.accountapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
