7ffeaca77ef7e2238fb238eafafd660ee4ac7865
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2022-02-08 16:01:54 -0500

    fixed circular beans dependency in StatsController

--- src/main/kotlin/com/tomoaki3284/accountapi/controller/StatsController.kt
+++ src/main/kotlin/com/tomoaki3284/accountapi/controller/StatsController.kt
@@ -1,4 +1,6 @@
 package com.tomoaki3284.accountapi.controller
+
+import com.tomoaki3284.accountapi.service.StatsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -10,11 +12,11 @@ import reactor.core.publisher.Mono
 @RequestMapping("/api/stats/v1")
 class StatsController {
 
-    private val statsController: StatsController
+    private val statsService: StatsService
 
     @Autowired
-    constructor(statsController: StatsController) {
-        this.statsController = statsController
+    constructor(statsService: StatsService) {
+        this.statsService = statsService
     }
 
     @GetMapping("/problems")
